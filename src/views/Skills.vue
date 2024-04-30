71a85189cfc63c9e205b86ab4e465a5060e79852
tofumaulana <161713454+tofumaulana@users.noreply.github.com>
2024-04-30 19:52:54 +0700

    remove skils

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -4,27 +4,27 @@
       <h1 class="font-bold text-2xl text-center md:pt-[10rem] pt-10">Skills</h1>
       <p class="text-center p-14 text-lg">"Kemampuan programming saya memungkinkan saya untuk mengembangkan aplikasi yang inovatif dan efisien."</p>
     </div>
-    <div class="bg-[#FFF6E3] h-[33rem]">
+    <div class="bg-[#FFF6E3] md:h-[33rem]">
       <div class="grid grid-cols-3 gap-4 p-10">
-        <div class="w-32 pt-5 pl-5">
+        <div class="w-32 md:pt-5 pt-1 md:pl-5">
           <img src="/image/img/logo-html.png" alt="" />
-          <h1 class="font-bold text-center text-xl pt-3">HTML</h1>
+          <h1 class="md:font-bold font-semibold text-center md:text-xl md:pt-3">HTML</h1>
         </div>
-        <div class="w-40">
+        <div class="md:w-40">
           <img src="/image/img/logo-css.png" alt="" />
-          <h1 class="font-bold text-center text-xl">CSS</h1>
+          <h1 class="md:font-bold font-semibold text-center md:text-xl">CSS</h1>
         </div>
-        <div class="w-40">
+        <div class="md:w-40">
           <img src="/image/img/logo-bootstrap.png" alt="" />
-          <h1 class="font-bold text-center text-xl">BOOTSTRAP</h1>
+          <h1 class="md:font-bold font-semibold text-center md:text-xl">BOOTSTRAP</h1>
         </div>
-        <div class="w-40 pt-16">
+        <div class="md:w-40 md:pt-16 pt-5">
           <img src="/image/img/logo-vuejs.png" alt="" />
-          <h1 class="font-bold text-center text-xl pt-3">VUE JS</h1>
+          <h1 class="md:font-bold font-semibold text-center md:text-xl md:pt-3">VUE JS</h1>
         </div>
-        <div class="w-40 pt-16">
+        <div class="md:w-40 md:pt-16 pt-5">
           <img src="/image/img/logo-tailwind.png" alt="" />
-          <h1 class="font-bold text-center text-xl pt-9">TAILWIND</h1>
+          <h1 class="md:font-bold font-semibold text-center md:text-xl md:pt-9">TAILWIND</h1>
         </div>
       </div>
     </div>
